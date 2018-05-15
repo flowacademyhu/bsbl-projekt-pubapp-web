b48accfe8974ca9d7c1a7537b131d4f745964a4e
Papp Gergely Flórián <papp.gergely.florian@gmail.com>
2018-05-15 22:31:19 +0200

    corrected the user model

--- src/main/java/org/flow/controllers/MainController.java
+++ src/main/java/org/flow/controllers/MainController.java
@@ -1,16 +1,39 @@
 package org.flow.controllers;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.flow.models.User;
+import org.flow.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping(path="/pubapp")
 public class MainController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping(path="/")
     public String render() {
         return "Hello there!";
+    }
+
+    @GetMapping(path="/add")
+    public @ResponseBody String addNewUser (@RequestParam String firstName, @RequestParam String lastName,
+                       @RequestParam String nickName, @RequestParam String email,
+                       @RequestParam @DateTimeFormat(pattern="yyyy-MM-dd") Date dob, @RequestParam Boolean gender) {
+
+        User newUser = new User();
+        newUser.setFirstName(firstName);
+        newUser.setLastName(lastName);
+        newUser.setNickName(nickName);
+        newUser.setEmail(email);
+        newUser.setDob(dob);
+        newUser.setGender(gender);
+        userRepository.save(newUser);
+        return "Saved";
     }
 }
--- src/main/java/org/flow/models/User.java
+++ src/main/java/org/flow/models/User.java
@@ -1,11 +1,10 @@
 package org.flow.models;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Id;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
@@ -13,19 +12,62 @@ public class User {
 
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name ="id", nullable = false)
     private Long id;
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false)
+    private Date created;
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at", nullable = false)
+    private Date updated;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "nick_name", nullable = false)
+    private String nickName;
+    @Column(name = "email", nullable = false)
     private String email;
+    @Column(name = "date_of_birth")
+    @DateTimeFormat(pattern="yyyy-MM-dd")
     private Date dob;
+    @Column(name = "gender")
     private Boolean gender;
 
-    public long getId() {
+    @PrePersist
+    protected void onCreate() {
+        updated = created = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updated = new Date();
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
     }
 
     public String getFirstName() {
@@ -44,6 +86,14 @@ public class User {
         this.lastName = lastName;
     }
 
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
     public String getEmail() {
         return email;
     }
@@ -60,11 +110,11 @@ public class User {
         this.dob = dob;
     }
 
-    public boolean isGender() {
+    public Boolean getGender() {
         return gender;
     }
 
-    public void setGender(boolean gender) {
+    public void setGender(Boolean gender) {
         this.gender = gender;
     }
 }
