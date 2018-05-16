33f6d80b6e2cd6d4e43743e8088dc604952743b6
Papp Gergely Flórián <papp.gergely.florian@gmail.com>
2018-05-16 10:15:11 +0200

    userachievement model half finished

--- src/main/java/org/flow/models/User.java
+++ src/main/java/org/flow/models/User.java
@@ -8,6 +8,7 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+@Table(name = "user")
 public class User {
 
     @Id
---
+++ src/main/java/org/flow/models/UserAchievement.java
@@ -0,0 +1,32 @@
+package org.flow.models;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "user_achievement")
+public class UserAchievement {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name ="id", nullable = false)
+    private Long id;
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false)
+    private Date created;
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at", nullable = false)
+    private Date updated;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "achievement_id")
+    //private Achievement achievement;
+}
