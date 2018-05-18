14ef4776b23da7afa2cab07d9b9b67536032dd09
Barna Rojik <rojikbarna@gmail.com>
2018-05-18 13:09:08 +0200

    user and achievements comtroller done

---
+++ src/main/java/org/flow/repositories/AchievementConditionRepository.java
@@ -0,0 +1,7 @@
+package org.flow.repositories;
+
+import org.flow.models.AchievementCondition;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AchievementConditionRepository extends CrudRepository<AchievementCondition, Long> {
+}
