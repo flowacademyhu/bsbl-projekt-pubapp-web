3f03d113b4c4903dc93d692bc63ad06f71dffe27
Papp Gergely Flórián <papp.gergely.florian@gmail.com>
2018-05-22 11:59:03 +0200

    minor changes

--- src/main/java/org/flow/repositories/AchievementConditionRepository.java
+++ src/main/java/org/flow/repositories/AchievementConditionRepository.java
@@ -1,7 +1,12 @@
 package org.flow.repositories;
 
+import org.flow.models.Achievement;
 import org.flow.models.AchievementCondition;
+import org.springframework.data.domain.Page;
 import org.springframework.data.repository.CrudRepository;
 
+import java.awt.print.Pageable;
+
 public interface AchievementConditionRepository extends CrudRepository<AchievementCondition, Long> {
+    Iterable<AchievementCondition> findByAchievementId(Long id, org.springframework.data.domain.Pageable pageable);
 }
