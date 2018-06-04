9cf0c3c3ba70cf083769fba9566e0ad880a23c87
Papp Gergely Flórián <papp.gergely.florian@gmail.com>
2018-06-05 00:57:42 +0200

    minor changes

--- src/main/java/org/flow/qr_code/QRCGenerator.java
+++ src/main/java/org/flow/qr_code/QRCGenerator.java
@@ -7,8 +7,6 @@ import com.google.zxing.client.j2se.MatrixToImageConfig;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
-import ij.IJ;
-import ij.ImagePlus;
 import org.apache.commons.lang.RandomStringUtils;
 import org.flow.models.Ordering;
 import org.flow.repositories.OrderingRepository;
